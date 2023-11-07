8740fb74aeec94c107935b9ae397855de29190ff
jorgedelpozolerida <jorgedelpozolerida@gmail.com>
2023-11-07 22:41:58 +0100

    tried region growing on CEREBRIU annotated dummy data

--- scripts/utils/utils_plotting.py
+++ scripts/utils/utils_plotting.py
@@ -19,13 +19,16 @@ import matplotlib.pyplot as plt
 import logging                                                                      # NOQA E402
 import numpy as np                                                                  # NOQA E402
 import pandas as pd                                                                 # NOQA E402
+import matplotlib.colors as mcolors
 
 
 logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger(__name__)
 
 
-def plot_mask_on_image(img_data, mask_data, cm_coords, alpha=0.5, title="", axes_to_display=['sagittal', 'coronal', 'axial'], interpolation='none'):
+def plot_mask_on_image(img_data, mask_data, cm_coords, alpha=0.5, title="",
+                        axes_to_display=['sagittal', 'coronal', 'axial'], 
+                        interpolation='none', cmap_custom=None):
     """
     Plot a mask on the corresponding image along specified axes, always displaying the axial view without a mask on the rightmost side.
     :param img_data: The 3D numpy array of the image data.
@@ -47,16 +50,23 @@ def plot_mask_on_image(img_data, mask_data, cm_coords, alpha=0.5, title="", axes
     num_axes = len(axes_to_display) + 1
     fig, axes = plt.subplots(1, num_axes, figsize=(5 * num_axes, 5))
     
+    # Create a color map for bright red overlay
+    if cmap_custom is None:
+        colors = [(0,0,0,0), (1,0,0,alpha)]  # from transparent to bright red
+        cmap = mcolors.LinearSegmentedColormap.from_list('custom_red', colors, N=2)
+    else:
+        cmap = cmap_custom
+
     # Dictionary to hold plane plotting functions
     plane_funcs = {
         'sagittal': lambda a: (a.imshow(img_data[x, :, :], cmap='gray'),
-                                a.imshow(mask_data[x, :, :], alpha=alpha, cmap='Reds', interpolation=interpolation),
+                                a.imshow(mask_data[x, :, :], alpha=alpha, cmap=cmap, interpolation=interpolation),
                                 a.set_title(f"Sagittal (x={x})")),
         'coronal': lambda a: (a.imshow(img_data[:, y, :], cmap='gray'),
-                                a.imshow(mask_data[:, y, :], alpha=alpha, cmap='Reds', interpolation=interpolation),
+                                a.imshow(mask_data[:, y, :], alpha=alpha, cmap=cmap, interpolation=interpolation),
                                 a.set_title(f"Coronal (y={y})")),
         'axial': lambda a: (a.imshow(img_data[:, :, z], cmap='gray'),
-                            a.imshow(mask_data[:, :, z], alpha=alpha, cmap='Reds', interpolation=interpolation),
+                            a.imshow(mask_data[:, :, z], alpha=alpha, cmap=cmap, interpolation=interpolation),
                             a.set_title(f"Axial (z={z})"))
     }
     
